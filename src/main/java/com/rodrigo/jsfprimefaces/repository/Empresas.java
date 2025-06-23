56e84496fe84cbf275348522ff2f4a61254d55f5
Rodrigo Soares <adsrodsoares@gmail.com>
2025-06-23 12:06:31 -0300

    Classe Empresas - métodos de manipulação (CRUD)

--- src/main/java/com/rodrigo/jsfprimefaces/repository/Empresas.java
+++ src/main/java/com/rodrigo/jsfprimefaces/repository/Empresas.java
@@ -1,5 +1,45 @@
 package com.rodrigo.jsfprimefaces.repository;
 
-public class Empresas {
+import java.io.Serializable;
+import java.util.List;
 
+import javax.persistence.TypedQuery;
+import javax.persistence.EntityManager;
+
+import com.rodrigo.jsfprimefaces.model.Empresa;
+
+public class Empresas implements Serializable {
+
+	public static final long serialVersionUID = 1L;
+	
+	private EntityManager manager;
+	
+	public Empresas() {
+		
+	}
+	
+	public Empresas(EntityManager manager) {
+		this.manager = manager;
+	}
+	
+	public Empresa porId(Long id) {
+		return manager.find(Empresa.class, id);
+	}
+	
+	public List<Empresa> pesquisar(String nome) {
+		TypedQuery<Empresa> query = manager.createQuery("from Empresa WHERE nomeFantasia like :nomeFantasia", Empresa.class);
+		query.setParameter("nomeFantasia", nome + "%");
+		return query.getResultList();
+	}
+	
+	//insert
+	public Empresa guardar(Empresa empresa) {
+		return manager.merge(empresa);
+	}
+	
+	public void remover(Empresa empresa) {
+		empresa = porId(empresa.getId());
+		manager.remove(empresa);
+		
+	}	
 }
